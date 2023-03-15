f553eadbaa78177c9201766561b5a6067c9fa344
Andriy Trofimov <webgrandmo@gmail.com>
2023-03-15 21:24:06 +0200

    Controller. Middleware and error handler

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const app = express();
+
+const PORT = dotenv.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/', (req, res) => {
+// 	res.status(200).json({ message: 'Welcome to the Support Desk API' });
+// });
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port : ${PORT}`);
+});
